5a3adefaeb40b39df832303c61d45b97556e68ef
Str1keWave <141131479+Str1keWave@users.noreply.github.com>
2025-10-01 13:13:44 -0400

    [EXP] defined basic logic for PCM conversion.js

--- pcm-processor.js
+++ pcm-processor.js
@@ -1,38 +1,30 @@
-class pcmProcessor extends AudioWorkletProcessor {
+class PCMProcessor extends AudioWorkletProcessor {
   process(inputs, outputs, parameters) {
-    // take the first output
-    const output = outputs[0];
-    // fill each channel with random values multiplied by gain
-    output.forEach((channel) => {
-      for (let i = 0; i < channel.length; i++) {
-        // generate random value for each sample
-        // Math.random range is [0; 1); we need [-1; 1]
-        // this won't include exact 1 but is fine for now for simplicity
-        channel[i] =
-          (Math.random() * 2 - 1) *
-          // the array can contain 1 or 128 values
-          // depending on if the automation is present
-          // and if the automation rate is k-rate or a-rate
-          (parameters["customGain"].length > 1
-            ? parameters["customGain"][i]
-            : parameters["customGain"][0]);
-      }
-    });
+
+    if (inputs[0].length === 0){
+      return true
+    }
+    const input = inputs[0][0];
+
+    //convert 32 -> 16
+    let int16Block = new Int16Array(input.length)
+
+    //scale each to the 16 int limit
+    for i in 0..floatBlock.length-1:
+      let v = floatBlock[i]
+      if (v > 1) v = 1
+      else if (v < -1) v = -1
+      // scale
+      int16Block[i] = (v < 0 ? v * 0x8000 : v * 0x7FFF)  // -32768..32767
+     this.port.postMessage({
+        type: "pcm16",
+        buffer: int16Block.buffer
+      }, [int16Block.buffer])
+  
     // as this is a source node which generates its own output,
     // we return true so it won't accidentally get garbage-collected
     // if we don't have any references to it in the main thread
     return true;
   }
-  // define the customGain parameter used in process method
-  static get parameterDescriptors() {
-    return [
-      {
-        name: "pcmProcessor",
-        defaultValue: 1,
-        minValue: 0,
-        maxValue: 1,
-        automationRate: "a-rate",
-      },
-    ];
-  }
-}
+  
+  registerProcessor('pcm-processor', PCMProcessor)
