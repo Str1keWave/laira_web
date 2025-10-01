d9f9641a944c9cdb09a3083c36730a132e9f222a
Str1keWave <141131479+Str1keWave@users.noreply.github.com>
2025-10-01 13:31:30 -0400

    [EXP] got minimal PCM server logic actually implemented, no idea if it actually works tho

--- pcm-processor.js
+++ pcm-processor.js
@@ -1,21 +1,27 @@
 class PCMProcessor extends AudioWorkletProcessor {
   process(inputs, outputs, parameters) {
 
+    //checks there's real audio to work with
+    if (inputs.length === 0){
+      return true}
     if (inputs[0].length === 0){
       return true
     }
+    
     const input = inputs[0][0];
 
     //convert 32 -> 16
     let int16Block = new Int16Array(input.length)
 
     //scale each to the 16 int limit
-    for i in 0..floatBlock.length-1:
-      let v = floatBlock[i]
+    for (let i = 0; i < input.length; i++){
+      let v = input[i]
       if (v > 1) v = 1
       else if (v < -1) v = -1
       // scale
       int16Block[i] = (v < 0 ? v * 0x8000 : v * 0x7FFF)  // -32768..32767
+    }
+    
      this.port.postMessage({
         type: "pcm16",
         buffer: int16Block.buffer
@@ -26,5 +32,8 @@ class PCMProcessor extends AudioWorkletProcessor {
     // if we don't have any references to it in the main thread
     return true;
   }
+
+}
+
+registerProcessor('pcm-processor', PCMProcessor)
   
-  registerProcessor('pcm-processor', PCMProcessor)
